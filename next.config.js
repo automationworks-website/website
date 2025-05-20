30620554137ecbf4ce348355bd849dd9f47626b1
InnovateTriumph <innovatetriumph@gmail.com>
2025-05-20 13:14:49 +0200

    changed config from ts to js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  // reactStrictMode: true, // Example: uncomment if you had this
+  // swcMinify: true,      // Example: uncomment if you had this
+};
+
+module.exports = nextConfig;
